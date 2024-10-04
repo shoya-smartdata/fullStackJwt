e029bdb72681156cb0adc3ed16c310a7ceff91d6
shoyab <shoyabkhan.smartdata@gmail.com>
2024-10-04 10:01:27 +0530

    test

--- jwtui/src/components/HeroSection/HomeComponent.js
+++ jwtui/src/components/HeroSection/HomeComponent.js
@@ -1,38 +1,9 @@
-// CarouselComponent.js
-import React from 'react';
-import Slider from 'react-slick';
-import './Crousoule.css'
+import React from 'react'
 
-const CarouselComponent = () => {
-  const cardsData = [
-    { id: 1, title: 'Card 1', content: 'This is the content for Card 1' },
-    { id: 2, title: 'Card 2', content: 'This is the content for Card 2' },
-    { id: 3, title: 'Card 3', content: 'This is the content for Card 3' },
-    { id: 4, title: 'Card 4', content: 'This is the content for Card 4' },
-    { id: 5, title: 'Card 5', content: 'This is the content for Card 5' },
-    { id: 6, title: 'Card 6', content: 'This is the content for Card 6' },
-  ];
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1, // Show one set of three cards per slide
-    slidesToScroll: 1,
-  };
-
+function HomeComponent() {
   return (
-    <Slider {...settings}>
-      {cardsData.map((card, index) => (
-        <div className="card-container" key={index}>
-          <div className="card">
-            <h3>{card.title}</h3>
-            <p>{card.content}</p>
-          </div>
-        </div>
-      ))}
-    </Slider>
-  );
-};
+    <div>HomeComponent</div>
+  )
+}
 
-export default CarouselComponent;
+export default HomeComponent
